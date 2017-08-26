c68753698cf10cdcf9f1e8dc356d0fb5e90f91e4
Tamik <hello@Tamik.ru>
2017-08-26 05:45:06 +0300

    Application: пример роутинга (закомментирован)

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 const Application = () => (
+  <div>
+    {/* <Route path='/'>
+      <div>
+        <Route path='/add' render={Indexed} />
+        <Route path='/add' component={Another} />
+      </div>
+    </Route>
+    <Route path='/feed'>
+      <Route path='/feed/add' render={Indexed} />
+    </Route> */}
+  </div>
 )
 
 export default Application
