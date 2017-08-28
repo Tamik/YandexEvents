15f745f9c0629621e9e3cb90c872fdd29f6cee87
Tamik <hello@Tamik.ru>
2017-08-28 20:40:55 +0300

    Добавлен экран Event (без стилизации)

--- src/js/screens/Event/Event.js
+++ src/js/screens/Event/Event.js
@@ -2,7 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import style from './Event.scss'
 
-const Event = () => (
+const data = {
+  title: 'Событие с Басковым',
+  description: 'Lorem ipsum dolor sit amet...',
+  image: {
+    large: 'http://placehold.it/250x250',
+  },
+}
 
+const Event = () => (
+  <div className='transition-item screen'>
+    <div className={`${style['page-inner']}`}>
+      <Link to='/feed'>Back</Link>
+      <br />
+      <img src={data.image.large} />
+      <h2>{data.title}</h2>
+      <p>{data.description}</p>
+    </div>
+  </div>
 )
+
 export default Event
