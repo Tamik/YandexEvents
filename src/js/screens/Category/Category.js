5389b63ad2b30daec21b615bd4c022b6a4718ba4
cybri0nix <cybri0nix19@gmail.com>
2017-08-29 18:42:26 +0300

    Возможность просмотра события из списка событий по категории

--- src/js/screens/Category/Category.js
+++ src/js/screens/Category/Category.js
@@ -3,6 +3,45 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import style from './style.scss'
 
+const payloadEventsListJSON = [
+  {
+    "id": 1,
+    "title": "Event 1",
+    "image": {
+      "small": {
+        "src": "https://img2.goodfon.ru/original/320x400/3/a0/daft-punk-daft-pank-tomas-2560.jpg"
+      }
+    }
+  },
+  {
+    "id": 2,
+    "title": "Event 2",
+    "image": {
+      "small": {
+        "src": "http://www.secureworldme.com/asset/images/portfolio/events.jpg"
+      }
+    }
+  },
+  {
+    "id": 3,
+    "title": "Event 3",
+    "image": {
+      "small": {
+        "src": "http://www.northcobbphotoclub.com/uploads/2/4/2/0/24207577/8109020_orig.jpg"
+      }
+    }
+  },
+  {
+    "id": 4,
+    "title": "Event 4",
+    "image": {
+      "small": {
+        "src": "http://www.northcobbphotoclub.com/uploads/2/4/2/0/24207577/4034355_orig.jpg"
+      }
+    }
+  },
+]
+
 const Category = ({ match }) => (
   <div className='transition-item screen'>
     <div className={`${style['page-inner']}`}>
@@ -19,12 +58,31 @@ const Category = ({ match }) => (
         - render
       */}
       <div>
-        event 1<br />
-        event 2<br />
-        event 3
+        <div className={style['events-list']}>
+          {
+            payloadEventsListJSON.map((item, index) => {
+              return (
+                <Link key={item.id} to='/event' onClick={this.viewEvent}>
+                  <div key={item.id} className={`${style['events-list__item']}`}>
+                    <div className={`${style['card-small__image-wrap']} ${style['image-fit-wrap']}`}>
+                      <img
+                        src={item.image.small.src}
+                        alt=''
+                        className={style['image-fit-wrap__image-fitted']}
+                      />
+                    </div>
+                    <div className={style['card-small__meta']}>
+                      <h3 className={`${style['events-list__item-title']}`}>{item.title}</h3>
+                    </div>
+                  </div>
+                </Link>
+              )
+            })
+          }
+        </div>
       </div>
     </div>
-  </div>
+  </div >
 )
 
 const mapStateToProps = store => ({
