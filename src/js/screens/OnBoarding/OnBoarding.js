756531af175c410fb1affa33d9afc6498f972074
cybri0nix <cybri0nix19@gmail.com>
2017-08-31 23:43:29 +0300

    OnBoarding: btn goNext handling and goto feed

--- src/js/screens/OnBoarding/OnBoarding.js
+++ src/js/screens/OnBoarding/OnBoarding.js
@@ -2,21 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-
+import { onBoardingViewed } from 'actions/userActions'
 import style from './style.scss'
 
-const OnBoarding = props => (
-  <div className={'transition-item screen'}>
-    <div className={style['page-inner']}>
-      <h1>Welcome to NewYear on Yandex.Afisha</h1>
-      <br /><br />
-      <Link to='/feed' style={{ background: '#fff', textDecoration: 'none', color: '#3c70c4' }} onClick={() => props.dispatch({ type: 'FIRST_LOG' })}>Продолжить</Link>
+const OnBoarding = (props) => {
+  const goNext = () => {
+    props.onGoNext()
+  }
+  return (
+    <div className={'transition-item screen'}>
+      <div className={style['page-inner']}>
+        <h1>Welcome to NewYear on Yandex.Afisha</h1>
+        <br /><br />
+        <button
+          style={{ background: '#fff', textDecoration: 'none', color: '#3c70c4' }}
+          onClick={goNext}
+        >
+          Продолжить
+        </button>
+      </div>
     </div>
-  </div>
-)
-
-OnBoarding.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  )
 }
 
-export default connect()(OnBoarding)
+export default connect(
+  state => ({
+    user: state.user,
+  }),
+  dispatch => ({
+    onGoNext: () => {
+      dispatch(onBoardingViewed())
+    },
+  })
+)(OnBoarding)
