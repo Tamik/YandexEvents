a3ba5074695941e58c6ea10f3e9074aa3708f14c
Belokuraya Angelina <belokurayaav@gmail.com>
2017-09-01 00:36:22 +0300

    Create screen Feed with ui-components

--- src/js/screens/Feed/Feed.js
+++ src/js/screens/Feed/Feed.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import { Container, Tabs } from 'ui-components'
+import { Tabs, Container, Slider, Carousel, Card, Image, Avatar } from 'ui-components'
 import style from './style.scss'
 import styleTabs from 'ui-components/Tabs/style.scss'
 
@@ -34,55 +34,89 @@ const payloadEventsListJSON = [
       }
     }
   },
-  {
-    "id": 4,
-    "title": "Event 4",
-    "image": {
-      "small": {
-        "src": "http://www.northcobbphotoclub.com/uploads/2/4/2/0/24207577/4034355_orig.jpg"
-      }
-    }
-  },
 ]
 
 const Feed = props => (
   <div className='transition-item screen'>
     <Tabs>
       <div className={`${styleTabs['tabs__item']} ${styleTabs['tabs__item_active']}`}>Лента</div>
-      <div className={styleTabs['tabs__item']}>Карта</div>
+      <div className={styleTabs.tabs__item}>Карта</div>
     </Tabs>
     <Container scrolling stretching>
-      {/* Block places */}
-      <div style={{ display: 'flex' }}>
-        <Link to='' style={{ display: 'block', width: 56, height: 56 }}>Place1</Link>
-        <Link to='' style={{ display: 'block', width: 56, height: 56 }}>Place2</Link>
-        <Link to='' style={{ display: 'block', width: 56, height: 56 }}>Place3</Link>
+      <Slider dots>
+        <div><Image size='large' src='http://krasivye-mesta.ru/img/Night-Moscow.jpg' /></div>
+        <div><Image size='large' src='http://andrive.ru/wp-content/uploads/2016/05/orig-600x386.jpg' /></div>
+        <div><Image size='large' src='http://lifeglobe.net/x/entry/472/2817506_large_3.jpg' /></div>
+      </Slider>
+      <Carousel>
+        <div style={{ margin: '16px 0' }}>
+          <div style={{ margin: '0 16px', padding: '23px', paddingBottom: '10px', backgroundColor: '#f2f2f2' }}>
+            <Avatar text='С детьми' size='medium' src='http://1.bp.blogspot.com/-z_oHLfIB4bQ/To2xXFpZwTI/AAAAAAAAUqo/iiGX6Su88Yw/s1600/pezinho5.png'/>
+          </div>
+          <div style={{ margin: '0 16px', padding: '23px', paddingBottom: '10px', backgroundColor: '#f2f2f2' }}>
+            <Avatar text='С детьми' size='medium' src='http://1.bp.blogspot.com/-z_oHLfIB4bQ/To2xXFpZwTI/AAAAAAAAUqo/iiGX6Su88Yw/s1600/pezinho5.png'/>
+          </div>
+          <div style={{ margin: '0 16px', padding: '23px', paddingBottom: '10px', backgroundColor: '#f2f2f2' }}>
+            <Avatar text='С детьми' size='medium' src='http://1.bp.blogspot.com/-z_oHLfIB4bQ/To2xXFpZwTI/AAAAAAAAUqo/iiGX6Su88Yw/s1600/pezinho5.png'/>
+          </div>
+          <div style={{ margin: '0 16px', padding: '23px', paddingBottom: '10px', backgroundColor: '#f2f2f2' }}>
+            <Avatar text='С детьми' size='medium' src='http://1.bp.blogspot.com/-z_oHLfIB4bQ/To2xXFpZwTI/AAAAAAAAUqo/iiGX6Su88Yw/s1600/pezinho5.png'/>
+          </div>
+        </div>
+      </Carousel>
+      <div style={{ marginLeft: 16 }}>
+        <Slider>
+          <Card size='medium' src='http://andrive.ru/wp-content/uploads/2016/05/orig-600x386.jpg' />
+          <Card size='medium' src='http://andrive.ru/wp-content/uploads/2016/05/orig-600x386.jpg' />
+          <Card size='medium' src='http://andrive.ru/wp-content/uploads/2016/05/orig-600x386.jpg' />
+        </Slider>
       </div>
-
       {/* Block events */}
       <div className={style['events-list']}>
         {
           payloadEventsListJSON.map((item, index) => {
             return (
               <Link key={item.id} to='/event' onClick={() => props.dispatch({ type: 'SEND_TO_EVENT', payload: item })}>
-                <div key={item.id} className={`${style['events-list__item']}`}>
-                  <div className={`${style['card-small__image-wrap']} ${style['image-fit-wrap']}`}>
-                    <img
-                      src={item.image.small.src}
-                      alt=''
-                      className={style['image-fit-wrap__image-fitted']}
-                    />
-                  </div>
-                  <div className={style['card-small__meta']}>
-                    <h3 className={`${style['events-list__item-title']}`}>{item.title}</h3>
-                  </div>
-                </div>
+                <Card key={item.id} size='small' />
               </Link>
             )
           })
         }
       </div>
-
+      <Carousel>
+        <div style={{ margin: '16px 0' }}>
+          <div style={{ margin: '0 16px' }}>
+            <Avatar text='Виктор Басков' size='small' src='http://1.bp.blogspot.com/-z_oHLfIB4bQ/To2xXFpZwTI/AAAAAAAAUqo/iiGX6Su88Yw/s1600/pezinho5.png'/>
+          </div>
+          <div style={{ margin: '0 16px' }}>
+            <Avatar text='Виктор Басков' size='small' src='http://1.bp.blogspot.com/-z_oHLfIB4bQ/To2xXFpZwTI/AAAAAAAAUqo/iiGX6Su88Yw/s1600/pezinho5.png'/>
+          </div>
+          <div style={{ margin: '0 16px' }}>
+            <Avatar text='Виктор Басков' size='small' src='http://1.bp.blogspot.com/-z_oHLfIB4bQ/To2xXFpZwTI/AAAAAAAAUqo/iiGX6Su88Yw/s1600/pezinho5.png'/>
+          </div>
+          <div style={{ margin: '0 16px' }}>
+            <Avatar text='Виктор Басков' size='small' src='http://1.bp.blogspot.com/-z_oHLfIB4bQ/To2xXFpZwTI/AAAAAAAAUqo/iiGX6Su88Yw/s1600/pezinho5.png'/>
+          </div>
+        </div>
+      </Carousel>
+      <div className={style['events-list']}>
+        {
+          payloadEventsListJSON.map((item, index) => {
+            return (
+              <Link key={item.id} to='/event' onClick={() => props.dispatch({ type: 'SEND_TO_EVENT', payload: item })}>
+                <Card key={item.id} size='small' />
+              </Link>
+            )
+          })
+        }
+      </div>
+      <div style={{ marginLeft: 16 }}>
+        <Slider>
+          <Card size='medium' src='http://andrive.ru/wp-content/uploads/2016/05/orig-600x386.jpg' />
+          <Card size='medium' src='http://andrive.ru/wp-content/uploads/2016/05/orig-600x386.jpg' />
+          <Card size='medium' src='http://andrive.ru/wp-content/uploads/2016/05/orig-600x386.jpg' />
+        </Slider>
+      </div>
       {/* Block categories */}
       <div style={{ display: 'flex' }}>
         <Link to='/category/1' style={{ display: 'block', width: 56, height: 56 }}>Cat1</Link>
