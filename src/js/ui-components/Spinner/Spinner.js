fdc075c212c09b69b1c23403070783af48005c02
Belokuraya Angelina <belokurayaav@gmail.com>
2017-09-01 17:19:12 +0300

    Add spinner

---
+++ src/js/ui-components/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import style from './style.scss'
+
+const Spinner = () => (
+  <div className={style.spinner}>
+    <div className={style['spinner__wrap']} />
+  </div>
+)
+
+export default Spinner
