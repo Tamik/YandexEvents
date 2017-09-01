0d3fda01ec5317d68351e5d87a371242b46e98f8
cybri0nix <cybri0nix19@gmail.com>
2017-09-01 13:35:25 +0300

    Screen Event connected to redux and have routing example inside

--- src/js/screens/Event/Event.js
+++ src/js/screens/Event/Event.js
@@ -1,22 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+
+import { push, goBack } from 'actions/navigationActions'
+
 import style from './style.scss'
 
-const Event = props => (
-  <div className='transition-item screen'>
-    <div className={`${style['page-inner']}`}>
-      <Link to='/feed'>Back</Link>
-      <br />
-      <img src={props.event.image.small.src} />
-      <h2>{props.event.title}</h2>
-      {/* <p>{props.event.description}</p> */}
-    </div>
-  </div>
-)
+class Event extends Component {
+  constructor(props) {
+    super(props)
 
-const mapStateToProps = store => ({
-  event: store.eventStore.event,
-})
+    if (!props.eventData) {
+      // Подгружаем данные с сервера
+      // Вот так можно взять eventId
+      console.log('eventData отсутствует в store')
+      console.log('eventId: ', props.params.id)
+    }
+    else {
+      // Покажем то что есть, а остальное подгрузим
+      console.log('eventData присутствует в store')
+      console.log('eventId: ', props.params.id)
+    }
 
-export default connect(mapStateToProps)(Event)
+    this.goBack = this.goBack.bind(this)
+  }
+
+  componentDidMount() {
+
+  }
+
+  goBack() {
+    this.props.goBack()
+  }
+
+  render() {
+    return (
+      <div className='transition-item screen'>
+        <div className={`${style['page-inner']}`}>
+          <button onClick={this.goBack}>GoBack</button>
+          <br />
+          <img src={'xxx'} />
+          <h2>event</h2>
+          {/* <p>{props.event.description}</p> */}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect(
+  state => ({
+    eventData: state.eventData,
+  }),
+  dispatch => ({
+    goBack: () => {
+      dispatch(goBack())
+    },
+  })
+)(Event)
