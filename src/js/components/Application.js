83076840514eafa2d310983c13186593a54f73a5
cybri0nix <cybri0nix19@gmail.com>
2017-09-01 13:38:19 +0300

    application is base redux routing handler now

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -4,19 +4,54 @@ import { connect } from 'react-redux'
 
 import style from './Application.scss'
 
-import { Main } from '../screens'
+import { Main, Event, Category } from '../screens'
 
 const Application = (props) => {
   const route = props.state.router.route
+  const routeChunks = window.location.hash.replace(/^#\/?|\/$/g, '').split('/')
 
-  if (route === '#/main') {
+  /*
+    @TODO: Match URLs properly
+  */
+
+  // Main -> Screen( OnBoarding | Feed )
+  if (!route || route === '#/main' || route === '#/' || route.indexOf('#/feed') > -1) {
     return <Main />
   }
-  if (route === '#/one') {
-    return <div>One</div>
+
+  // Screen Map
+  if (route.indexOf('#/map') > -1) { 
+    // here
   }
-  return <div>Not found</div>
+
+  // Screen Category
+  if (route.indexOf('#/category') > -1) {
+    const id = route.split('/')[2]
+    const params = { id }
+    return <Category params={params} />
+  }
+
+  // Screen Place
+  if (route.indexOf('#/place') > -1) {
+    const id = route.split('/')[2]
+    const params = { id }
+    /* @todo: change to Place */
+    return <div>PlaceId is {id}, but "screen/Place" not found, implement it here<br /><br /><strong>For going back, press GoBack button in your browser ;)</strong></div>
+  }
+
+  // Screen Event
+  if (route.indexOf('#/event') > -1) {
+    const id = route.split('/')[2]
+    const params = { id }
+    return <Event params={params} />
+  }
+
+  // @todo: redirect to Screen Main
+  return <div>Not found ;)</div>
 }
 
-export default connect(state => ({ state }))(Application)
+export default connect(
+  state => ({ state }),
+  dispatch => ({})
+)(Application)
 
