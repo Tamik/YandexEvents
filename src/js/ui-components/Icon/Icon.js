7c1f0ef5246ed3c6a1bd6593627faa40f509c835
Belokuraya Angelina <belokurayaav@gmail.com>
2017-09-03 21:42:48 +0300

    Add icons

--- src/js/ui-components/Icon/Icon.js
+++ src/js/ui-components/Icon/Icon.js
@@ -11,13 +11,27 @@ const Icon = (props) => {
       return <svg viewBox='0 0 24 24' width={props.width} height={props.height} color={props.color} dangerouslySetInnerHTML={{ __html: IconsList.map }} />
     case 'list':
       return <svg viewBox='0 0 24 24' width={props.width} height={props.height} color={props.color} dangerouslySetInnerHTML={{ __html: IconsList.list }} />
+    case 'star':
+      return <svg viewBox='0 0 22 21' width={props.width} height={props.height} color={props.color} dangerouslySetInnerHTML={{ __html: IconsList.star }} />
+    case 'starActive':
+      return <svg viewBox='0 0 22 21' width={props.width} height={props.height} color={props.color} dangerouslySetInnerHTML={{ __html: IconsList.starActive }} />
+    case 'profile':
+      return <svg viewBox='0 0 22 20' width={props.width} height={props.height} color={props.color} dangerouslySetInnerHTML={{ __html: IconsList.profile }} />
+    case 'search':
+      return <svg viewBox='0 0 18 18' width={props.width} height={props.height} color={props.color} dangerouslySetInnerHTML={{ __html: IconsList.search }} />
+    case 'ticket':
+      return <svg viewBox='0 0 16 20' width={props.width} height={props.height} color={props.color} dangerouslySetInnerHTML={{ __html: IconsList.ticket }} />
+    case 'moscowDay':
+      return <svg viewBox='0 0 22 22' width={props.width} height={props.height} color={props.color} dangerouslySetInnerHTML={{ __html: IconsList.moscowDay }} />
+    case 'moscowDayActive':
+      return <svg viewBox='0 0 20 20' width={props.width} height={props.height} color={props.color} dangerouslySetInnerHTML={{ __html: IconsList.moscowDayActive }} />
     default:
       return <p>Нет такой иконки</p>
   }
 }
 
 Icon.propTypes = {
-  type: PropTypes.string,
+  type: PropTypes.string.isRequired,
   width: PropTypes.number,
   height: PropTypes.number,
   color: PropTypes.string,
