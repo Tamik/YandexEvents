138d1e3c1380bc70882198113307bb692c64cf89
Belokuraya Angelina <belokurayaav@gmail.com>
2017-09-05 01:49:15 +0300

    Add new styles for Avatar

--- src/js/ui-components/Avatar/Avatar.js
+++ src/js/ui-components/Avatar/Avatar.js
@@ -3,15 +3,14 @@ import PropTypes from 'prop-types'
 import style from './style.scss'
 
 const Avatar = props => (
-  <div>
+  <div className={style.avatar}>
     <div
-      className={`${style.avatar} ${style[`avatar_${props.size}`]}`}
+      className={style.avatar__img}
       style={{ backgroundImage: `url(${props.src})` }}
     />
-    { props.text
-      ? <p className={style['avatar__text']}>{props.text}</p>
-      : null
-    }
+    <div className={style.avatar__description}>
+      <p className={style.avatar__title}>{props.title}</p>
+    </div>
   </div>
 )
 
