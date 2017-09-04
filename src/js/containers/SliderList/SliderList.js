6a0ee87d754e3859d87a23d49576c45d908418ee
Belokuraya Angelina <belokurayaav@gmail.com>
2017-09-05 01:54:34 +0300

    Add title for slider

--- src/js/containers/SliderList/SliderList.js
+++ src/js/containers/SliderList/SliderList.js
@@ -2,17 +2,21 @@ import React from 'react'
 
 import { List } from 'components'
 import { Slider } from 'ui-components'
+import style from 'components/List/style.scss'
 
 const SliderList = props => (
-  <Slider>
-    {
-      props.payload.map(item => (
-        <div>
-          <List key={item.id} type='slider_events' data={item} />
-        </div>
-      ))
-    }
-  </Slider>
+  <div>
+    <h3 className={style.list__title}>{props.title}</h3>
+    <Slider>
+      {
+        props.payload.map(item => (
+          <div>
+            <List key={item.id} type='slider_events' data={item} />
+          </div>
+        ))
+      }
+    </Slider>
+  </div>
 )
 
 export default SliderList
