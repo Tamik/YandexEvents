6c51795dd4bbbef375bbfbeb1337cffd528b0e8b
Tamik <hello@Tamik.ru>
2017-09-05 23:28:45 +0300

    Feed: рефакторинг компонента

--- src/js/screens/Feed/Feed.js
+++ src/js/screens/Feed/Feed.js
@@ -1,41 +1,6 @@
 import React from 'react'
 
-import { EventsList, SliderList, AvatarsList } from 'containers'
+const Feed = () => (
+)
 
-
-class Feed extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      data: [],
-      sliderData: [],
-      carouselData: [],
-    }
-  }
-
-  componentDidMount() {
-    axios.get('http://io.yamblz.ru/events', { params: { items_per_page: 3 } })
-      .then(response => this.setState({ data: response.data.data }))
-    axios.get('http://io.yamblz.ru/events', { params: { items_per_page: 3, page: 2 } })
-      .then((response) => {
-        response.data.data[0].title = 'Парк Горького'
-        response.data.data[1].title = 'Парк Сокольники'
-        response.data.data[2].title = 'ВДНХ'
-        this.setState({ sliderData: response.data.data })
-      })
-  }
-
-  render() {
-    return (
-      <div style={{ paddingBottom: 68 }}>
-        <EventsList payload={this.state.data} />
-        <SliderList title='Любимые места москвичей' payload={this.state.sliderData} />
-        <EventsList payload={this.state.data} />
-        <AvatarsList payload={this.state.data} />
-      </div>
-    )
-  }
-}
-
-export default connect()(Feed)
+export default Feed
