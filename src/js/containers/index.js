1471fc303d41cb18a433b780c3818985e7034c3e
Tamik <hello@Tamik.ru>
2017-09-05 23:36:41 +0300

    Настроен экспорт контейнеров

--- src/js/containers/index.js
+++ src/js/containers/index.js
@@ -1,2 +1,6 @@
 export { default as EventsList } from './EventsList'
 export { default as SliderList } from './SliderList'
+export { default as List } from './List'
+export { default as Carousel } from './Carousel'
+export { default as Slider } from './Slider'
+export { default as Hint } from './Hint'
