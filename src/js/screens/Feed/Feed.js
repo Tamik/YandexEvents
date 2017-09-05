77bc64c136b70824357539d3c65e26c0985884e4
cybri0nix <cybri0nix19@gmail.com>
2017-09-05 16:51:29 +0300

    hardcoded places titles

--- src/js/screens/Feed/Feed.js
+++ src/js/screens/Feed/Feed.js
@@ -23,14 +23,19 @@ class Feed extends Component {
     axios.get('http://io.yamblz.ru/events', { params: { items_per_page: 3 } })
       .then(response => this.setState({ data: response.data.data }))
     axios.get('http://io.yamblz.ru/events', { params: { items_per_page: 3, page: 2 } })
-      .then(response => this.setState({ sliderData: response.data.data }))
+      .then((response) => {
+        response.data.data[0].title = 'Парк Горького'
+        response.data.data[1].title = 'Парк Сокольники'
+        response.data.data[2].title = 'ВДНХ'
+        this.setState({ sliderData: response.data.data })
+      })
   }
 
   render() {
     return (
       <div style={{ paddingBottom: 68 }}>
         <EventsList payload={this.state.data} />
-        <SliderList title='Любимые места москвичей' payload={this.state.data} />
+        <SliderList title='Любимые места москвичей' payload={this.state.sliderData} />
         <EventsList payload={this.state.data} />
         <AvatarsList payload={this.state.data} />
       </div>
