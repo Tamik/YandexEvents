a60bbae089517e88b3530590e079a6ab6df3a11a
Tamik <hello@Tamik.ru>
2017-09-05 23:26:20 +0300

    OnBoarding: удален import react-router-dom

--- src/js/screens/OnBoarding/OnBoarding.js
+++ src/js/screens/OnBoarding/OnBoarding.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+
 import { onBoardingViewed } from 'actions/userActions'
 import { replace } from 'actions/navigationActions'
 
 import { Container } from 'ui-components'
+
 import style from './style.scss'
 
 const OnBoarding = (props) => {
