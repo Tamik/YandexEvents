bc8001ff626acd3e54d60c3b75d630a2406967da
cybri0nix <cybri0nix19@gmail.com>
2017-09-07 23:51:55 +0300

    Replaced plain bottomnav to component

--- src/js/screens/Event/Event.js
+++ src/js/screens/Event/Event.js
@@ -4,9 +4,12 @@ import { connect } from 'react-redux'
 
 import { push, goBack } from 'actions/navigationActions'
 
-import { TopBar, Image, Icon } from 'ui-components'
-import style from './style.scss'
+import { TopBar, Image, Icon, Container } from 'ui-components'
+import { BottomNav } from 'components'
 import styleCard from 'ui-components/Card/style.scss'
+
+import styleBotNav from 'ui-components/BottomNavigation/style.scss'
+import style from './style.scss'
 
 class Event extends Component {
   constructor(props) {
@@ -34,27 +37,34 @@ class Event extends Component {
 
   render() {
     return (
-      <div>
+      <div className='screen'>
         <TopBar
           isTransparent
           icon={<button onClick={this.goBack}>
             <Icon type='arrowBack' width='24' height='24' color='#fff' />
           </button>}
         />
-        <Image size='large' src={`http://io.yamblz.ru/i/events/${this.props.eventData.id}_large.jpg`} />
-        <div className={`${styleCard.card__info} ${style.card__info_large}`}>
-          <h2 className={`${styleCard.card__title} ${styleCard.card__title_large}`}>
-            {this.props.eventData.title}
-          </h2>
-          <div style={{ marginBottom: 16, fontSize: '1.25rem', lineHeight: '1.75rem', color: '#000', }}>
-            <p>{`${this.props.eventData.dateFormatted.day} - ${this.props.eventData.dateEndFormatted.day}, ${this.props.eventData.dateEndFormatted.month} `}</p>
-            <p>{`${this.props.eventData.dateFormatted.time} - ${this.props.eventData.dateEndFormatted.time}`}</p>
-          </div>
-          <p
-            style={{ fontSize: '1rem', lineHeight: '1.375rem', color: '#000' }}
-            dangerouslySetInnerHTML={{ __html: this.props.eventData.description }}
-          />
-        </div>
+        <Container scrolling stretching>
+          {this.props.eventData
+            ? <Image size='large' src={`http://io.yamblz.ru/i/events/${this.props.eventData.id}_large.jpg`} />
+            : ''}
+          {this.props.eventData
+            ? <div className={`${styleCard.card__info} ${style.card__info_large}`}>
+              <h2 className={`${styleCard.card__title} ${styleCard.card__title_large}`}>
+                {this.props.eventData.title}
+              </h2>
+              <div style={{ marginBottom: 16, fontSize: '1.25rem', lineHeight: '1.75rem', color: '#000', }}>
+                <p>{`${this.props.eventData.dateFormatted.day} - ${this.props.eventData.dateEndFormatted.day}, ${this.props.eventData.dateEndFormatted.month} `}</p>
+                <p>{`${this.props.eventData.dateFormatted.time} - ${this.props.eventData.dateEndFormatted.time}`}</p>
+              </div>
+              <p
+                style={{ fontSize: '1rem', lineHeight: '1.375rem', color: '#000' }}
+                dangerouslySetInnerHTML={{ __html: this.props.eventData.description }}
+              />
+            </div>
+            : ''}
+        </Container>
+        <BottomNav />
       </div>
     )
   }
