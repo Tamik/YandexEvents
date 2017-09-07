3b8e338a3f466ab94573a25d740c8a19ffb589b4
Belokuraya Angelina <belokurayaav@gmail.com>
2017-09-07 22:00:50 +0300

    Add icon in floatingButton

--- src/js/ui-components/FloatingButton/FloatingButton.js
+++ src/js/ui-components/FloatingButton/FloatingButton.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { Icon } from 'ui-components'
 import style from './style.scss'
 
 const FloatingButton = props => (
   <button className={style.button} onClick={props.onClick}>
-    {props.label}
+    <Icon type={props.typeIcon} height='24' color='#1e1367' />
+    {props.title}
   </button>
 )
 
 FloatingButton.propTypes = {
-  label: PropTypes.node,
+  title: PropTypes.string,
+  typeIcon: PropTypes.string,
   onClick: PropTypes.func,
 }
 
