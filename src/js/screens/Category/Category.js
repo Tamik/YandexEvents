75d2db935cd147f1a81d6d66653d4f85000fdc05
cybri0nix <cybri0nix19@gmail.com>
2017-09-08 05:41:10 +0300

    Filtering events by dates in category (except for category)

--- src/js/screens/Category/Category.js
+++ src/js/screens/Category/Category.js
@@ -7,6 +7,7 @@ import { setViewMode } from 'actions/viewActions'
 import { VIEW_MODE_LIST, VIEW_MODE_MAP } from 'consts/viewModes'
 
 import { FloatingButton } from 'ui-components'
+import { DataApi } from 'utils/DataApi'
 
 import { List as ListContainer } from 'containers'
 import { Map } from 'screens'
@@ -17,17 +18,36 @@ class Category extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
-
     this.viewMode = this.props.params.viewMode.toUpperCase()
 
-    // if (!props.categoryData) {
-    //   // load data from server by categoryId
-    //   console.log('Load events from server by category id: ', props)
-    // }
-    // else {
-    //   console.log('categoryData: ', props.categoryData)
-    // }
+    this.state = {
+      filterByDate: null,
+      holiDates: [],
+    }
+  }
+
+  componentWillMount() {
+    this.getHoliDates(this.props)
+  }
+
+  componentWillUpdate(nextProps) {
+    const isCategoryEqual = this.props.params.categoryId === nextProps.params.categoryId
+
+    if (!isCategoryEqual) {
+      this.getHoliDates(nextProps)
+    }
+  }
+
+  getHoliDates(props) {
+    DataApi.getDatesWithEvents()
+      .byHoliday(1)
+      .byCategory(props.params.categoryId)
+      .perform()
+      .then((response) => {
+        this.setState({
+          holiDates: response.data.data,
+        })
+      })
   }
 
   toggleViewMode = () => {
@@ -38,12 +58,48 @@ class Category extends Component {
     )
   }
 
+  filterByDate = (date) => {
+    if (date !== this.state.filterByDate) {
+      this.setState({
+        filterByDate: date,
+      })
+      return
+    }
+    this.setState({
+      filterByDate: null,
+    })
+  }
+
   render() {
     return (
       <div>
         {
           this.viewMode === VIEW_MODE_LIST
-            ? <ListContainer categoryId={this.props.params.categoryId} />
+            ? <div>
+              {
+                this.state.holiDates.length > 1
+                  ? <p style={{ padding: '16px 16px 24px 16px' }}>
+                    {this.state.holiDates.map((item) => {
+                      return (
+                        <span
+                          key={item.date}
+                          role='button'
+                          className={`${style.filter_date} ${this.state.filterByDate === item.date ? style.filter_date__active : ''}`}
+                          onClick={() => {
+                            this.filterByDate(item.date)
+                          }}
+                        >{item.dateFormatted.day} {item.dateFormatted.month}</span>
+                      )
+                    })}
+                  </p>
+                  : ''
+              }
+
+              <ListContainer
+                categoryId={this.props.params.categoryId}
+                filterByDate={this.state.filterByDate}
+              />
+            </div>
             : <Map categoryId={this.props.params.categoryId} />
         }
         <FloatingButton
@@ -58,6 +114,7 @@ class Category extends Component {
 export default connect(
   state => ({
     categoryData: state.data.categoryData,
+    config: state.data.configData,
     view: state.view,
   }),
   dispatch => ({
