61f62afebd8ae704c6eefff3bb90bdc7fd62015c
Tamik <hello@Tamik.ru>
2017-09-09 15:47:20 +0300

    Обновлены стили в контейнере AvatarsList

--- src/js/containers/AvatarsList/AvatarsList.js
+++ src/js/containers/AvatarsList/AvatarsList.js
@@ -7,7 +7,16 @@ import style from 'components/List/style.scss'
 
 const AvatarsList = props => (
   <div className={style.list__wrap}>
-    <h3 className={style.list__title}>{props.title}</h3>
+    {props.title
+      ? <h3
+        style={{
+          fontSize: '1rem',
+          margin: '36px 16px 8px',
+          lineHeight: '1.25rem',
+        }}
+      >{props.title}</h3>
+      : null
+    }
     <Slider>
       {
         props.payload.map(item => (
