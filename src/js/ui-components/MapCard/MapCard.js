b8bd43aede1df86c50ab352624eeb8ed488d7172
Tamik <hello@Tamik.ru>
2017-09-09 15:52:06 +0300

    Обновлен ui-компонент MapCard

--- src/js/ui-components/MapCard/MapCard.js
+++ src/js/ui-components/MapCard/MapCard.js
@@ -4,8 +4,12 @@ import { Image } from 'ui-components'
 import style from './style.scss'
 
 const MapCard = props => (
-  <div className={`${style.card} ${style[`card_${props.size}`]}`} onClick={props.onClick}>
-    <Image src={props.src} size={props.size} />
+  <div
+    className={`${style.card} ${style[`card_${props.size}`]}`}
+    onClick={props.onClick}
+    role='button'
+  >
+    <Image src={props.src} size={props.size} style={{ height: 128 - 32 }} />
     <div className={` ${style.card__info} ${style[`card__info_${props.size}`]}`}>
       <h3 className={` ${style.card__title} ${style[`card__title_${props.size}`]}`}>
         {props.title}
