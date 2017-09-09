db13f16aa68fea824a65438909743c1c3dbf73df
Tamik <hello@Tamik.ru>
2017-09-09 17:23:12 +0300

    Рефакторинг контейнера AvatarsList

--- src/js/containers/AvatarsList/AvatarsList.js
+++ src/js/containers/AvatarsList/AvatarsList.js
@@ -1,32 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-import { List } from 'components'
-import { Slider } from 'ui-components'
+import { push } from 'actions/navigationActions'
+import { sendModalEventData } from 'actions/dataActions'
+
+import { Avatar, Slider } from 'ui-components'
 
 import style from 'components/List/style.scss'
 
-const AvatarsList = props => (
-  <div className={style.list__wrap}>
-    {props.title
-      ? <h3
-        style={{
-          fontSize: '1rem',
-          margin: '36px 16px 8px',
-          lineHeight: '1.25rem',
-        }}
-      >{props.title}</h3>
-      : null
-    }
-    <Slider>
-      {
-        props.payload.map(item => (
-          <div>
-            <List key={item.id} type='slider_avatars' data={item} />
-          </div>
-        ))
-      }
-    </Slider>
-  </div>
-)
+class AvatarsList extends Component {
+  componentDidMount() {
+    console.warn('Component AvatarsList is deprecated.')
+  }
 
-export default AvatarsList
+  viewEvent = (route, item) => {
+    this.props.onViewEvent(route, item)
+  }
+
+  render() {
+    return (
+      <div className={style.list__wrap}>
+        {this.props.title
+          ? <h3
+            style={{
+              fontSize: '1rem',
+              margin: '36px 16px 8px',
+              lineHeight: '1.25rem',
+            }}
+          >{this.props.title}</h3>
+          : null
+        }
+        <Slider>
+          {
+            this.props.payload.map(item => (
+              <Avatar
+                key={item.id}
+                src={item.photo_small}
+                title={item.title}
+                onClick={() => this.viewEvent('/entity/%', item)}
+              />
+            ))
+          }
+        </Slider>
+      </div>
+    )
+  }
+}
+
+export default connect(
+  state => ({}),
+  dispatch => ({
+    onViewEvent: (route, element) => {
+      dispatch(sendModalEventData(element))
+      dispatch(push(route.replace('%', element.id)))
+    },
+  })
+)(AvatarsList)
