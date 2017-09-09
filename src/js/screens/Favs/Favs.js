c897fde77c5aca7064c8aa90f41b63c0716b9a80
cybri0nix <cybri0nix19@gmail.com>
2017-09-09 14:09:55 +0300

    Real list and topbar, but without map

--- src/js/screens/Favs/Favs.js
+++ src/js/screens/Favs/Favs.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { push, goBack } from 'actions/navigationActions'
+import { sendModalEventData } from 'actions/dataActions'
 
-import { TopBar, Image, Icon, Container } from 'ui-components'
+import { TopBar, Image, Icon, Container, Card } from 'ui-components'
 import { BottomNav } from 'components'
 
 import { DataApi } from 'utils/DataApi'
@@ -12,34 +13,49 @@ import { DataApi } from 'utils/DataApi'
 import styleBotNav from 'ui-components/BottomNavigation/style.scss'
 import style from './style.scss'
 
-class Favs extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      holidays: [],
-    }
+const Favs = (props) => {
+  const viewEvent = (eventData) => {
+    props.onViewEvent(eventData)
   }
-
-  componentDidMount() {
-
-  }
-
-  render() {
-    return (
-      <div className='screen'>
-        <Container scrolling stretching>
-          Список избранных событий
-        </Container>
-        <BottomNav />
-      </div>
-    )
-  }
+  return (
+    <div className='screen'>
+      <TopBar
+        title='Закладки'
+      />
+      <Container scrolling stretching>
+        <div style={{ margin: 16 }}>
+          {
+            Object.keys(props.favs).length
+              ? props.favs.map(item => (
+                <Card
+                  key={item.id}
+                  title={item.title}
+                  src={`${item.photo_small}`}
+                  location={item.location_title}
+                  size='medium'
+                  style={{
+                    marginBottom: 20,
+                  }}
+                  onClick={() => viewEvent(item)}
+                  date={`${item.dateFormatted.day} ${item.dateFormatted.month} ${item.dateFormatted.time} `}
+                />
+              ))
+              : <p style={{ textAlign: 'center', fontSize: 14 }}>Здесь будут ваши закладки</p>}
+        </div>
+      </Container>
+      <BottomNav />
+    </div>
+  )
 }
 
 export default connect(
   state => ({
-    /* favs: state.user.favs */
+    favs: state.user.favs || {},
   }),
   dispatch => ({
+    onViewEvent: (eventData) => {
+      dispatch(sendModalEventData(eventData))
+      dispatch(push(`/event/${eventData.id}`))
+    },
   })
 )(Favs)
