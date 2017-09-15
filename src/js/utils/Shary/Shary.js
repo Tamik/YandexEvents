6d41c88d47ae700f88be751af293e7be91e46f8a
cybri0nix <cybri0nix19@gmail.com>
2017-09-15 03:51:19 +0300

    added Shary util (wrap on cordova plugin) for sharing

---
+++ src/js/utils/Shary/Shary.js
@@ -0,0 +1,40 @@
+const CHOOSER_TITLE_EVENT = 'Поделиться событием'
+const EVENT_URL_TPL = `${process.env.HOST}/event/%`
+
+/**
+ * @see https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin#readme
+ */
+
+const share = options => new Promise((resolve, reject) => {
+  window.plugins.socialsharing.shareWithOptions(options, resolve, reject)
+})
+
+const Shary = {
+  shareEvent: (eventData) => {
+    const enter = eventData.is_free ? 'Свободный вход' : ''
+    const messageText = [
+      `${eventData.title}, `,
+      `${eventData.dateTime}, `,
+      (enter ? `${enter}, ` : ''),
+      (eventData.location_title !== eventData.address
+        ? `${eventData.location_title} (${eventData.address})`
+        : ''),
+    ].join('')
+
+    const options = {
+      message: messageText,
+      subject: eventData.title,
+      files: [eventData.photo_large],
+      url: EVENT_URL_TPL.replace(/%/, eventData.id),
+      chooserTitle: CHOOSER_TITLE_EVENT,
+    }
+
+    return share(options)
+  },
+
+  sharePlace: () => { },
+  shareEntity: () => { },
+  shareApp: () => { },
+}
+
+export default Shary
