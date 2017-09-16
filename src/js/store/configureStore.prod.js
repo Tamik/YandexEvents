877c92c75282b9178aef389fd5b12a336f2aaa93
Tamik <hello@Tamik.ru>
2017-09-16 18:11:12 +0300

    Обновлены createStore

--- src/js/store/configureStore.prod.js
+++ src/js/store/configureStore.prod.js
@@ -1,11 +1,24 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { createHashHistory } from 'history'
+import localforage from 'localforage'
 
 import rootReducer from 'reducers/rootReducer'
 
-export default function configureStore() {
-  return createStore(
+import routerMiddleware from 'middlewares/routerMiddleware'
+import storageMiddleware from 'middlewares/storageMiddleware'
+
+const history = createHashHistory({ hashType: 'slash' })
+
+const configureStore = () => (
+  createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    applyMiddleware(
+      routerMiddleware(history),
+      storageMiddleware(localforage),
+      thunk,
+    )
   )
-}
+)
+
+export default configureStore
