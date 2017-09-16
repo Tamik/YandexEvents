83996668363f0852fc93f768c8656d53f58a0b30
Tamik <hello@Tamik.ru>
2017-09-16 16:11:05 +0300

    Произведен рефакторинг контейнера Hint
    а так же обновлена документация

--- src/js/containers/Hint/Hint.js
+++ src/js/containers/Hint/Hint.js
@@ -1,44 +1,61 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import { fetchContainerData } from 'actions/constructorActions'
 
 import { Spinner } from 'ui-components'
-
-import { DataApi } from 'utils'
 
 /**
  * @class HintContainer
  * @description Компонент-контейнер для конструктора ленты
  */
-export default class HintContainer extends Component {
-  state = {
-    loading: true,
-    image: '',
+class HintContainer extends Component {
+  /**
+   * @static propTypes
+   */
+  static propTypes = {
+    onRequestData: PropTypes.func.isRequired,
+    params: PropTypes.shape().isRequired,
+    style: PropTypes.shape(),
+  }
+
+  /**
+   * @static defaultProps
+   * @description Значения props по-умолчанию
+   */
+  static defaultProps = {
+    style: {},
   }
 
   componentWillMount() {
-    DataApi.getHint().byHoliday(1)
-      .perform()
-      .then(response => this.setState({
-        image: response.data.data,
-        loading: false,
-      }))
+    this.getData(this.props)
   }
+
+  /**
+   * @method getData
+   * @description Вызов экшена запроса данных для контейнера
+   * @param {Object} props
+   */
+  getData = props => this.props.onRequestData(props, this.props.params.key)
 
   render() {
     const heightImage = Math.floor((400 / 360) * window.innerWidth)
 
     return (
       <div style={this.props.style}>
-        {this.state.loading
-          ? (<Spinner />)
-          : (
+        {this.props.constructor[this.props.params.key]
+          ? (
             <div
               style={{
                 width: '100%',
                 height: heightImage,
               }}
-              dangerouslySetInnerHTML={{ __html: this.state.image }}
+              dangerouslySetInnerHTML={{ __html: this.props.constructor[this.props.params.key] }}
             />
+          )
+          : (
+            <Spinner />
           )
         }
       </div>
@@ -46,10 +63,13 @@ export default class HintContainer extends Component {
   }
 }
 
-HintContainer.defaultProps = {
-  style: {},
-}
-
-HintContainer.propTypes = {
-  style: PropTypes.shape(),
-}
+export default connect(
+  state => ({
+    constructor: state.constructor,
+  }),
+  dispatch => ({
+    onRequestData: (payload, key) => {
+      dispatch(fetchContainerData(payload.params, key))
+    },
+  })
+)(HintContainer)
